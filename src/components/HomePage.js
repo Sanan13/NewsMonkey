4e763201e4cac3c74624f280366f9e52e458e593
saiyed10 <saiyedsanan10@gmail.com>
2023-11-17 20:19:34 +0530

    final commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+function HomePage() {
+  const navigate = useNavigate();
+  const goToNews=()=>{
+    navigate("/business")
+  }
+  return (
+    <div>
+      <div class="myHome">
+        <h2>Welcome to</h2>
+        <h1>NewsMonkey</h1>
+        <h3>&ldquo;Stay Informed, Stay Empowered&rdquo;</h3>
+        <div class="myText">
+            <p>Welcome to our news app, where you'll discover a world of information at your fingertips. Stay up-to-date
+                with breaking news, diverse perspectives, and empowering stories that matter most to you. Get informed
+                and inspired every day with our easy-to-use, personalized news experience.</p>
+        </div>
+        <div class="myButtons">
+            <button onClick={goToNews} style={{fontSize:"18px",fontWeight:"600"}}>Go to News &rarr;</button>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default HomePage
