0849e99a19f591286046ce40ea4d16c66d22b197
Sanan13 <saiyedsanan9@gmail.com>
2024-11-01 21:57:44 +0530

    class changed to className

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,16 +7,16 @@ function HomePage() {
   }
   return (
     <div>
-      <div class="myHome">
+      <div className="myHome">
         <h2>Welcome to</h2>
         <h1>NewsMonkey</h1>
         <h3>&ldquo;Stay Informed, Stay Empowered&rdquo;</h3>
-        <div class="myText">
+        <div className="myText">
             <p>Welcome to our news app, where you'll discover a world of information at your fingertips. Stay up-to-date
                 with breaking news, diverse perspectives, and empowering stories that matter most to you. Get informed
                 and inspired every day with our easy-to-use, personalized news experience.</p>
         </div>
-        <div class="myButtons">
+        <div className="myButtons">
             <button onClick={goToNews} style={{fontSize:"18px",fontWeight:"600"}}>Go to News &rarr;</button>
         </div>
     </div>
